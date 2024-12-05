a590a02de1fa8f0df4b87ed26f6885ec4af6caf6
KeelanS <keelan@live.be>
2024-12-05 16:47:00 +0100

    add ci

--- libs/internal-plugin/src/executors/fly-deploy/executor.ts
+++ libs/internal-plugin/src/executors/fly-deploy/executor.ts
@@ -4,13 +4,13 @@ import { execSync } from 'child_process';
 
 const runExecutor: PromiseExecutor<FlyDeployExecutorSchema> = async (options) => {
   const cwd = options.distLocation;
-  const results = execSync(`/Users/keelan/.fly/bin/flyctl apps list`);
+  const results = execSync(`fly apps list`);
   try {
     if (results.toString().includes(options.flyAppName)) {
-      execSync(`/Users/keelan/.fly/bin/flyctl deploy`, { cwd, stdio: 'inherit' });
+      execSync(`fly deploy`, { cwd, stdio: 'inherit' });
     } else {
       // consult https://fly.io/docs/reference/regions/ to get best region for you
-      execSync(`/Users/keelan/.fly/bin/flyctl launch --now --name=${options.flyAppName} --yes --copy-config --region=lax`, {
+      execSync(`fly launch --now --name=${options.flyAppName} --yes --copy-config --region=lax`, {
         cwd,
         stdio: 'inherit',
       });
